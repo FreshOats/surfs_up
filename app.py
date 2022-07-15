aea556c13423b9e002e53649fd2a4929f827189e
Justin Papreck <justin.papreck@gmail.com>
2022-07-15 12:22:46 -0700

    Added more flask routes

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def welcome():
     /api/v1.0/stations
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
-    ''')
+    ''') 
 
 
 @app.route("/api/v1.0/precipitation")
@@ -47,3 +47,48 @@ def precipitation():
     precip = {date: prcp for date, prcp in precipitation}
     
     return jsonify(precip) 
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+
+    stations = list(np.ravel(results))
+
+    return jsonify(stations = stations)
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.tobs). \
+        filter(Measurement.station == 'USC00519281'). \
+        filter(Measurement.date >= prev_year).all()
+
+    temps = list(np.ravel(results))
+
+    return jsonify(temps=temps)
+
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0//temp/<start>/<end>")
+
+def stats(start=None, end=None):
+
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end: 
+        results = session.query(*sel). \
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+
+        return jsonify(temps=temps)
+
+    results = session.query(*sel). \
+        filter(Measurement.date >= start). \
+        filter(Measurement.date <= end).all()
+
+    temps = list(np.ravel(results))
+
+    return jsonify(temps)
